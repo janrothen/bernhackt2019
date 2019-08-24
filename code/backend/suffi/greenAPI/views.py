18739cf7f909c619d8bddb3364a845795056097e
Pascal Grüter <pascal.grueter@protonmail.ch>
2019-08-24 23:18:12 +0200

    Added Trophy Url and Serializer

--- code/backend/suffi/greenAPI/views.py
+++ code/backend/suffi/greenAPI/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 
 from django.contrib.auth.models import User, Group
 from rest_framework import viewsets
-from .serializers import ActiveChallengeSerializer, UserSerializer, GroupSerializer, CategorySerializer, ChallengeSerializer, OptionSerializer
-from .models import Category, Challenge, Option, ActiveChallenge
+from .serializers import TrophySerializer, ActiveChallengeSerializer, UserSerializer, GroupSerializer, CategorySerializer, ChallengeSerializer, OptionSerializer
+from .models import Category, Challenge, Option, ActiveChallenge, Trophy
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -25,10 +25,17 @@ class ChallengeViewSet(viewsets.ModelViewSet):
     queryset = Challenge.objects.all()
     serializer_class = ChallengeSerializer
 
+
 class OptionViewSet(viewsets.ModelViewSet):
     queryset = Option.objects.all()
     serializer_class = OptionSerializer
 
+
 class ActiveChallengeViewSet(viewsets.ModelViewSet):
     queryset = ActiveChallenge.objects.all()
     serializer_class = ActiveChallengeSerializer
+
+
+class TrophyViewSet(viewsets.ModelViewSet):
+    queryset = Trophy.objects.all()
+    serializer_class = TrophySerializer
--- code/backend/suffi/urls.py
+++ code/backend/suffi/urls.py
@@ -25,6 +25,7 @@ router.register(r'category', views.CategoryViewSet)
 router.register(r'challenge', views.ChallengeViewSet)
 router.register(r'option', views.OptionViewSet)
 router.register(r'activeChallenge', views.ActiveChallengeViewSet)
+router.register(r'trophy', views.TrophyViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
