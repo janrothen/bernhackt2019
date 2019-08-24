21942243ae57dd5a2b86cb6c21e89cd0b2555405
Holzchopf <oli4stuker@hotmail.com>
2019-08-24 14:45:02 +0200

    fix 2 for category

--- code/frontend/suffi/src/app/pages/category/category.component.ts
+++ code/frontend/suffi/src/app/pages/category/category.component.ts
@@ -7,30 +7,23 @@ import { Router, ActivatedRoute, ParamMap } from '@angular/router';
   styleUrls: ['./category.component.scss']
 })
 export class CategoryComponent implements OnInit {
-
+  private challenges: any = {};
 
   constructor(
     private route: ActivatedRoute
   ) { }
 
   ngOnInit() {
-    let id: number = +this.route.snapshot.paramMap.get('id');
-    // no id given, take main challenge categories
-    if (!id) {
-      this.challenges = this.getChallenges([1,2,3,4]);
-    // id given, extract its childs
-    } else {
-      // selected challenge
-      let sel = this.getChallenges([id])[0];
-      this.challenges = this.getChallenges(sel.childs);
-    }
-  }
-
-  // return all the challenges from challengesstore with given ids
-  private getChallenges(ids: number[]) {
-    return this.challengesstore.filter((challenge) => {
-     return ids.indexOf(challenge.id) >= 0 ? true : false;
-   });
+    // let id: number = +this.route.snapshot.paramMap.get('id');
+    // // no id given, take main challenge categories
+    // if (!id) {
+    //   this.challenges = this.getChallenges([1,2,3,4]);
+    // // id given, extract its childs
+    // } else {
+    //   // selected challenge
+    //   let sel = this.getChallenges([id])[0];
+    //   this.challenges = this.getChallenges(sel.childs);
+    // }
   }
 
 }
