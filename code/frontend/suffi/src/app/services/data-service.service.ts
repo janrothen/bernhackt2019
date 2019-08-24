6a1418d92a372eca63b19b10f5ddf85eccabdd73
Holzchopf <oli4stuker@hotmail.com>
2019-08-24 15:17:56 +0200

    moved data definitions to service

--- code/frontend/suffi/src/app/services/data-service.service.ts
+++ code/frontend/suffi/src/app/services/data-service.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
 
 @Injectable({
   providedIn: 'root'
@@ -8,15 +9,19 @@ export class DataServiceService {
   constructor() { }
 
   public getCategories(): Category[] {
-    return {
-
-    } as Category[];
+    return [
+      {id: 1, label: "Nahrung", icon: ['fas', 'utensils'], solid: true, challenges: []},
+      {id: 2, label: "Verkehr", icon: ['fas', 'car'], solid: true, challenges: []},
+      {id: 3, label: "Wohnen", icon: ['fas', 'home'], solid: false, challenges: []},
+      {id: 4, label: "Kleidung", icon: ['fas', 'tshirt'], solid: false, challenges: []}
+    ] as Category[];
   }
 }
 
 // base class for Category and Challenge (so they can be displayed with same
 // components, e.g. as button)
 class Categorised {
+  public id: number;
   public icon: any;
   public label: string;
   public help: number = 0;
--- code/frontend/suffi/src/app/structure/challenge-button/challenge-button.component.ts
+++ code/frontend/suffi/src/app/structure/challenge-button/challenge-button.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { Category, Challenge } from '../../services/data-service.service';
 
 import { faQuestionCircle } from '@fortawesome/free-solid-svg-icons';
 
@@ -8,13 +9,19 @@ import { faQuestionCircle } from '@fortawesome/free-solid-svg-icons';
   styleUrls: ['./challenge-button.component.scss']
 })
 export class ChallengeButtonComponent implements OnInit {
-  @Input() challenge: any;
+  @Input() challenge: Category | Challenge;
+  private route: string;
 
   faQuestionCircle = faQuestionCircle
 
   constructor() { }
 
   ngOnInit() {
+    console.log(this.challenge);
+    this.route = '/challenge';
+    if (this.challenge as Category) {
+      this.route = '/category';
+    }
   }
 
 }
